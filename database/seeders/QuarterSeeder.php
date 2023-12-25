93a90c677053239b7eaf483661a2899492b154a0
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2023-12-25 23:41:11 +0800

    final config for quarters seeder

--- database/seeders/QuarterSeeder.php
+++ database/seeders/QuarterSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use DB;
-
 class QuarterSeeder extends Seeder
 {
     /**
@@ -15,12 +14,12 @@ class QuarterSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['quarter' => 1, 'active' => 1],
-            ['quarter' => 2],
+            ['quarter' => 1, 'active' => 1], // First quarter set to active: 1
+            ['quarter' => 2], // Rest of the quarters will use default value (likely 0)
             ['quarter' => 3],
             ['quarter' => 4]
         ];
 
-        DB::table('quarters')->upsert($data, ['quarter'], ['quarter']);
+        DB::table('quarters')->upsert($data, ['quarter'], ['quarter', 'active']);
     }
 }

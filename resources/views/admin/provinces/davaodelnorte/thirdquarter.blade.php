2d25eb1e571dd346f22dc7bbe5721336e454d7a3
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-01-03 11:02:34 +0800

    changed the page title

--- resources/views/admin/provinces/davaodelnorte/thirdquarter.blade.php
+++ resources/views/admin/provinces/davaodelnorte/thirdquarter.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard</title>
+    <title>Davao Del Norte Third Quarter</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
--- resources/views/admin/provinces/davaodelsur/secondquarter.blade.php
+++ resources/views/admin/provinces/davaodelsur/secondquarter.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard</title>
+    <title>Davao Del Sur Second Quarter</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">

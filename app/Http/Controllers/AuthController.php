3d1710a688b93d3ed3c62810eeb3c26bcc718091
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-01-14 22:32:24 +0800

    working variance report [partially]

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,26 +61,58 @@ class AuthController extends Controller
             ];
             // assign the usertypemap
             $user_type = $userTypeMap[$user->user_type] ?? null;
+
+            // map to get previous quarter 
+            $quarterMapping = [
+                1 => 4,
+                2 => 1,
+                3 => 2,
+                4 => 3,
+            ];
+    
+            // get the current active quarter for the current year
+            $current_active_quarter = DB::table('quarters')->where('active', 1)->first();
+    
+            // assign the map
+            $previous_quarter = $quarterMapping[$current_active_quarter->quarter];
+
+
             $previous_quarter_result = DB::table('reports')
             ->select(DB::raw('SUM(total_physical_count) as total_physical_count'), DB::raw('SUM(total_budget_utilized) as total_budget_utilized'))
-            ->where('quarter_id', 4)
-            ->where('year', 2023)
-            ->where('program_id', 2)
+            ->where('quarter_id', $previous_quarter)
+            ->where('year', Carbon::now()->year)
+            ->where('program_id', $user_type)
             ->first();
+
             $previous_program_target = DB::table('program_targets')
-            ->where('quarter_id', 4)
-            ->where('year', 2023)
-            ->where('program_id', 2)
+            ->where('quarter_id', $previous_quarter)
+            ->where('year', Carbon::now()->year)
+            ->where('program_id', $user_type)
             ->first();
+
+            $variance_check = DB::table('variance_submission_check')
+            ->where('quarter_id', $previous_quarter)
+            ->where('program_id', $user_type)
+            ->where('year', Carbon::now()->year)
+            ->where('submitted', 1)
+            ->first();
+
             // budget achieved
-            $isBudgetAchieved = ($previous_quarter_result->total_budget_utilized >= $previous_program_target->budget_target);
+            $isBudgetAchieved = ($previous_quarter_result->total_budget_utilized == $previous_program_target->budget_target);
+            
             // physical target achieved
-            $isPhysicalCountAchieved = ($previous_quarter_result->total_physical_count >= $previous_program_target->physical_target);
+            $isPhysicalCountAchieved = ($previous_quarter_result->total_physical_count == $previous_program_target->physical_target);
+            
             // check if both are true
             $isAchieved = $isBudgetAchieved && $isPhysicalCountAchieved;
-            if (!($isAchieved) && $user->user_type !== UserTypeEnum::ADMIN) {
+            
+            // for fresh deployment in production servers
+            $count_reports = DB::table('reports')->count();
+            
+            
+            if (!($isAchieved) && ($user->user_type !== UserTypeEnum::ADMIN) && !$variance_check && !$count_reports == 0) {
                 return view('client.variance');
-            } else {
+            } else  {
                 return redirect('/client/dashboard');
             }
         }
